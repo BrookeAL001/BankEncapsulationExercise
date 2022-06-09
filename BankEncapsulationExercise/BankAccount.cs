bdc2014e6fbf8b7b36e9a0fc48ad388e89fcb995
BrookeAL0012 <bbcecil17@gmail.com>
2022-06-09 12:47:06 -0500

    initial commit

---
+++ BankEncapsulationExercise/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulationExercise
+{
+    public class BankAccount
+    {
+        private double Balance;
+
+        public void Deposit(double depositAmount)
+        {
+            Console.WriteLine($"Depositing an amount of {depositAmount, 0:c} into your account");
+            Balance = depositAmount;
+        }
+
+        public double GetBalance()
+        {
+            return Balance;
+        }
+       
+    }
+}
